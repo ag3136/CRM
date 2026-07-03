d4053e7879e8315da01292b648f9db63f9395e64
Modernize Studio <studio@modernize.com>
2026-07-03 09:25:15 +0000

    Commit for planId: 235

--- src/main/java/crm/utils/WriteCsvToResponse.java
+++ src/main/java/crm/utils/WriteCsvToResponse.java
@@ -18,15 +18,13 @@ public class WriteCsvToResponse {
 
     public static void writeCustomers(PrintWriter printWriter, List<Customer> customers) {
         try {
-            ColumnPositionMappingStrategy columnPositionMappingStrategy = new ColumnPositionMappingStrategy();
-
+            ColumnPositionMappingStrategy<Customer> columnPositionMappingStrategy = new ColumnPositionMappingStrategy<>();
             columnPositionMappingStrategy.setType(Customer.class);
-            columnPositionMappingStrategy.generateHeader();
 
             String[] columns = new String[]{"id", "name", "email", "phone", "firstName", "lastName", "city", "address", "enabled"};
             columnPositionMappingStrategy.setColumnMapping(columns);
 
-            StatefulBeanToCsv statefulBeanToCsv = new StatefulBeanToCsvBuilder(printWriter)
+            StatefulBeanToCsv<Customer> statefulBeanToCsv = new StatefulBeanToCsvBuilder<Customer>(printWriter)
                     .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
                     .withMappingStrategy(columnPositionMappingStrategy)
                     .withSeparator(',')
@@ -40,14 +38,13 @@ public class WriteCsvToResponse {
 
     public static void writeCustomer(PrintWriter printWriter, Customer customer) {
         try {
-            ColumnPositionMappingStrategy columnPositionMappingStrategy = new ColumnPositionMappingStrategy();
-
+            ColumnPositionMappingStrategy<Customer> columnPositionMappingStrategy = new ColumnPositionMappingStrategy<>();
             columnPositionMappingStrategy.setType(Customer.class);
 
             String[] columns = new String[]{"id", "name", "email", "phone", "firstName", "lastName", "city", "address", "enabled"};
             columnPositionMappingStrategy.setColumnMapping(columns);
 
-            StatefulBeanToCsv statefulBeanToCsv = new StatefulBeanToCsvBuilder(printWriter)
+            StatefulBeanToCsv<Customer> statefulBeanToCsv = new StatefulBeanToCsvBuilder<Customer>(printWriter)
                     .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
                     .withMappingStrategy(columnPositionMappingStrategy)
                     .withSeparator(',')
