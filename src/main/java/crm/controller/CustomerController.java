a1012bfdd19e609153b4841024c2ad36cdfd82ec
Modernize Studio <studio@modernize.com>
2026-08-18 09:35:31 +0000

    Commit for planId: 267

--- src/main/java/crm/controller/CustomerController.java
+++ src/main/java/crm/controller/CustomerController.java
@@ -5,9 +5,12 @@ import crm.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
--- src/main/java/crm/controller/PdfController.java
+++ src/main/java/crm/controller/PdfController.java
@@ -12,8 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.validation.Valid;
 
-import javax.validation.Valid;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
